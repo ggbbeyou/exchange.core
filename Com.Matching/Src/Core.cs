e8fa66e8e67cbbe9e4e3b363cdf9fbb6f3b0d4bf
ma.feng <ma.feng@shuweikeji.com>
2021-05-25 15:46:41 +0800

    a

--- Com.Matching/Src/Core.cs
+++ Com.Matching/Src/Core.cs
@@ -130,30 +130,7 @@ namespace Com.Matching
             this.configuration = configuration;
             this.mq = new MQ(this);
 
-            // ConnectionFactory factory = new ConnectionFactory() { HostName = "192.168.1.3", Port = 5672, UserName = "guest", Password = "guest" };
-            // IConnection connection = factory.CreateConnection();
-            // this.channel = connection.CreateModel();
-
-            // channel.ExchangeDeclare(exchange: "PendingOrder", type: "topic");
-            // string queueName = channel.QueueDeclare().QueueName;
-            // channel.QueueBind(queue: queueName, exchange: "PendingOrder", routingKey: this.name);
-            // EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
-
-            // consumer.Received += (model, ea) =>
-            //                     {
-            //                         var body = ea.Body.ToArray();
-            //                         var message = Encoding.UTF8.GetString(body);
-            //                         var routingKey = ea.RoutingKey;
-            //                         Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
-            //                     };
-            // channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
-            // // channel.Close();
-            // // connection.Close();
-
-
-            // //ShutdownEventArgs args = new ShutdownEventArgs();
-            // consumer.HandleModelShutdown(this.channel, null);
-            // //Process();
+            
         }
 
         /// <summary>
--- Com.Matching/Src/MQ.cs
+++ Com.Matching/Src/MQ.cs
@@ -14,6 +14,33 @@ namespace Com.Matching
         public MQ(Core core)
         {
             this.core = core;
+
+
+            // ConnectionFactory factory = new ConnectionFactory() { HostName = "192.168.1.3", Port = 5672, UserName = "guest", Password = "guest" };
+            // IConnection connection = factory.CreateConnection();
+            // this.channel = connection.CreateModel();
+
+            // channel.ExchangeDeclare(exchange: "PendingOrder", type: "topic");
+            // string queueName = channel.QueueDeclare().QueueName;
+            // channel.QueueBind(queue: queueName, exchange: "PendingOrder", routingKey: this.name);
+            // EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            // consumer.Received += (model, ea) =>
+            //                     {
+            //                         var body = ea.Body.ToArray();
+            //                         var message = Encoding.UTF8.GetString(body);
+            //                         var routingKey = ea.RoutingKey;
+            //                         Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+            //                     };
+            // channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+            // // channel.Close();
+            // // connection.Close();
+
+
+            // //ShutdownEventArgs args = new ShutdownEventArgs();
+            // consumer.HandleModelShutdown(this.channel, null);
+            // //Process();
+            
         }
 
     }

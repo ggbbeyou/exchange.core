73950c536ed712d623b59e915fdb48e87f35c7b7
mf <fengma_312@163.com>
2021-06-19 10:49:19 +0800

    a

--- Com.Api/Startup.cs
+++ Com.Api/Startup.cs
@@ -28,7 +28,7 @@ namespace Com.Api
             RedisConfiguration redisConfiguration = Configuration.GetSection("Redis").Get<RedisConfiguration>();
             services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(redisConfiguration);
             services.AddControllersWithViews();
-            services.AddHostedService<MainService>();
+            // services.AddHostedService<MainService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

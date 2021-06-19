9eee2f3787d2420e90ef383cd217bdd39afa58c6
mf <fengma_312@163.com>
2021-06-19 10:27:23 +0800

    a

--- Com.Api/Controllers/InteriorController.cs
+++ Com.Api/Controllers/InteriorController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Com.Api.Models;
+using RabbitMQ.Client;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace Com.Api.Controllers
 {
@@ -14,20 +17,82 @@ namespace Com.Api.Controllers
     /// </summary>
     public class InteriorController : Controller
     {
-        private readonly ILogger<InteriorController> _logger;
+        private readonly ILogger<InteriorController> logger;
 
-        public InteriorController(ILogger<InteriorController> logger)
+        /// <summary>
+        /// 配置接口
+        /// </summary>
+        public readonly IConfiguration configuration;
+        public readonly IConnection connection;
+
+        public InteriorController(IConfiguration configuration, ILogger<InteriorController> logger)
         {
-            _logger = logger;
+            this.configuration = configuration;
+            this.logger = logger;
+            ConnectionFactory factory = this.configuration.GetSection("RabbitMQ").Get<ConnectionFactory>();
+            this.connection = factory.CreateConnection();
         }
 
         public IActionResult Index()
         {
+
             return View();
         }
 
-        
+        /// <summary>
+        /// 启动撮合服务
+        /// </summary>
+        /// <param name="service_name">撮合服务器名称</param>
+        /// <param name="name">交易对</param>
+        /// <param name="price">最后成交价</param>
+        /// <returns></returns>
+        public IActionResult MatchingStart(string service_name, string name, decimal price)
+        {
+            string queue_name = $"MatchingService";
+            string comman = $"open:{service_name}:{name}:{price}";
+            byte[] body = Encoding.UTF8.GetBytes(comman);
+            try
+            {
+                IModel channel = this.connection.CreateModel();
+                channel.QueueDeclare(queue: queue_name, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                IBasicProperties basicProperties = channel.CreateBasicProperties();
+                basicProperties.Persistent = true;
+                channel.BasicPublish(exchange: "", routingKey: queue_name, basicProperties: basicProperties, body: body);
+                channel.Close();
+            }
+            catch (System.Exception)
+            {
+                return Json(false);
+            }
+            return Json(true);
+        }
 
-       
+        /// <summary>
+        /// 关闭撮合服务
+        /// </summary>
+        /// <param name="service_name">撮合服务器名称</param>
+        /// <param name="name">交易对</param>
+        /// <returns></returns>
+        public IActionResult MatchingStop(string service_name, string name)
+        {
+            string queue_name = $"MatchingService";
+            string comman = $"close:{service_name}:{name}";
+            byte[] body = Encoding.UTF8.GetBytes(comman);
+            try
+            {
+                IModel channel = this.connection.CreateModel();
+                channel.QueueDeclare(queue: queue_name, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                IBasicProperties basicProperties = channel.CreateBasicProperties();
+                basicProperties.Persistent = true;
+                channel.BasicPublish(exchange: "", routingKey: queue_name, basicProperties: basicProperties, body: body);
+                channel.Close();
+            }
+            catch (System.Exception)
+            {
+                return Json(false);
+            }
+            return Json(true);
+        }
+
     }
 }

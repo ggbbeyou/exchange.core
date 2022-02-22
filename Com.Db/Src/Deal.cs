31b2388be30cbd722650e221c7db73d976bf8fd0
fengma <fengma_312@126.com>
2022-02-22 16:31:54 +0800

    a

--- Com.Db/Src/Deal.cs
+++ Com.Db/Src/Deal.cs
@@ -13,5 +13,11 @@ public class Deal : BaseDeal
     /// 时间戳(分钟)
     /// </summary>
     /// <value></value>
-    public long timestamp { get; set; }
+    public long timestamp
+    {
+        get
+        {
+            return this.time.AddSeconds(-this.time.Second).AddMilliseconds(-this.time.Millisecond).ToUnixTimeSeconds();
+        }
+    }
 }

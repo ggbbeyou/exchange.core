cdc4e7c39e01370bb5b80509cdfbd3d319fa665d
fengma <fengma_312@126.com>
2022-02-22 15:59:09 +0800

    a

--- Com.Db/Src/Deal.cs
+++ Com.Db/Src/Deal.cs
@@ -7,56 +7,7 @@ namespace Com.Db;
 /// <summary>
 /// 成交单
 /// </summary>
-public class Deal
+public class Deal : BaseDeal
 {
-    /// <summary>
-    /// 成交id
-    /// </summary>
-    /// <value></value>
-    public long trade_id { get; set; }
-    /// <summary>
-    /// 交易对
-    /// </summary>
-    /// <value></value>
-    public string market { get; set; } = null!;
-    /// <summary>
-    /// 成交价
-    /// </summary>
-    /// <value></value>
-    public decimal price { get; set; }
-    /// <summary>
-    /// 成交量
-    /// </summary>
-    /// <value></value>
-    public decimal amount { get; set; }
-    /// <summary>
-    /// 成交总额
-    /// </summary>
-    /// <value></value>
-    public decimal total { get; set; }
-    /// <summary>
-    /// 成交触发方向
-    /// </summary>
-    /// <value></value>
-    public E_OrderSide trigger_side { get; set; }
-    /// <summary>
-    /// 成交时间
-    /// </summary>
-    /// <value></value>
-    public DateTimeOffset time { get; set; }
-    /// <summary>
-    /// 买订单
-    /// </summary>
-    /// <value></value>
-    public long bid { get; set; }
-    /// <summary>
-    /// 卖订单
-    /// </summary>
-    /// <value></value>
-    public long ask { get; set; }
-    /// <summary>
-    /// 分钟时间戳(秒)
-    /// </summary>
-    /// <value></value>
-    public long timestamp { get; set; }
+
 }

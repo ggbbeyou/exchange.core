6ae2ff099767f439abfdd161a118de60f0a1dc04
fengma_312 <fengma_312@126.com>
2022-03-06 21:49:04 +0800

    a

--- Com.Web/Controllers/ServiceController.cs
+++ Com.Web/Controllers/ServiceController.cs
@@ -31,57 +31,5 @@ public class ServiceController : Controller
         return View();
     }
 
-    /// <summary>
-    /// 启动撮合服务
-    /// </summary>
-    /// <param name="service_name">撮合服务器名称</param>
-    /// <param name="name">交易对</param>
-    /// <param name="price">最后成交价</param>
-    /// <returns></returns>
-    public IActionResult MatchingStart(string service_name, string name, decimal price)
-    {
-        string queue_name = $"MatchingService";
-        string comman = $"open:{service_name}:{name}:{price}";
-        byte[] body = Encoding.UTF8.GetBytes(comman);
-        try
-        {
 
-            constant.i_model.QueueDeclare(queue: queue_name, durable: true, exclusive: false, autoDelete: false, arguments: null);
-            IBasicProperties basicProperties = this.constant.i_model.CreateBasicProperties();
-            basicProperties.Persistent = true;
-            this.constant.i_model.BasicPublish(exchange: "", routingKey: queue_name, basicProperties: basicProperties, body: body);
-            this.constant.i_model.Close();
-        }
-        catch (System.Exception)
-        {
-            return Json(false);
-        }
-        return Json(true);
-    }
-
-    /// <summary>
-    /// 关闭撮合服务
-    /// </summary>
-    /// <param name="service_name">撮合服务器名称</param>
-    /// <param name="name">交易对</param>
-    /// <returns></returns>
-    public IActionResult MatchingStop(string service_name, string name)
-    {
-        string queue_name = $"MatchingService";
-        string comman = $"close:{service_name}:{name}";
-        byte[] body = Encoding.UTF8.GetBytes(comman);
-        try
-        {
-            this.constant.i_model.QueueDeclare(queue: queue_name, durable: true, exclusive: false, autoDelete: false, arguments: null);
-            IBasicProperties basicProperties = this.constant.i_model.CreateBasicProperties();
-            basicProperties.Persistent = true;
-            this.constant.i_model.BasicPublish(exchange: "", routingKey: queue_name, basicProperties: basicProperties, body: body);
-            this.constant.i_model.Close();
-        }
-        catch (System.Exception)
-        {
-            return Json(false);
-        }
-        return Json(true);
-    }
 }

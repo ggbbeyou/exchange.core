b7f3e33420287dc899f2461329a586eaddf243ef
fengma_312 <fengma_312@126.com>
2022-03-06 22:30:14 +0800

    a

--- Com.Model/Enum/E_Op.cs
+++ Com.Model/Enum/E_Op.cs
@@ -20,32 +20,24 @@ public enum E_Op
     /// </summary>
     service_stop = 2,
     /// <summary>
-    /// 启动交易对撮合
-    /// </summary>
-    match_start = 3,
-    /// <summary>
-    /// 停止交易对撮合
-    /// </summary>
-    match_stop = 4,
-    /// <summary>
     /// 挂单
     /// </summary>
-    place = 5,
+    place = 3,
     /// <summary>
     /// 根据订单id撤单
     /// </summary>
-    cancel_by_id = 6,
+    cancel_by_id = 4,
     /// <summary>
     /// 根据用户id撤单
     /// </summary>
-    cancel_by_uid = 7,
+    cancel_by_uid = 5,
     /// <summary>
     /// 根据客户自定义id撤单
     /// </summary>
-    cancel_by_clientid = 8,
+    cancel_by_clientid = 6,
     /// <summary>
     /// 该交易对全部撤单
     /// </summary>
-    cancel_by_all = 9,
+    cancel_by_all = 7,
 
 }

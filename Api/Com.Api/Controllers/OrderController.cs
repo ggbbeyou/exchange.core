c51b99c8c62f33b04fea0fd5dee8975f495012e1
fengma_312 <fengma_312@126.com>
2022-03-08 22:50:39 +0800

    a

---
+++ Api/Com.Api/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Com.Api.Models;
+
+namespace Com.Api.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public OrderController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}

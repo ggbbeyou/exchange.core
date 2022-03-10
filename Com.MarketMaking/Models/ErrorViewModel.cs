66356f43d33e26f6438e8158f87ba84d54f9cb91
fengma_312 <fengma_312@126.com>
2022-03-10 21:53:30 +0800

    a

--- Com.MarketMaking/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Com.MarketMaking.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}

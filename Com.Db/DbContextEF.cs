dac3f2d11bb0dd8c0f344060bc7d8a2bfe3b38e8
fengma_312 <fengma_312@126.com>
2022-03-13 12:18:28 +0800

    a

--- Com.Db/DbContextEF.cs
+++ Com.Db/DbContextEF.cs
@@ -48,7 +48,7 @@ public class DbContextEF : DbContext
         modelBuilder.Entity<MarketInfo>(o =>
         {
             o.HasKey(p => p.market);
-            o.Property(P => P.market).IsRequired().HasColumnType("bigint").HasMaxLength(50).HasComment("交易对");
+            o.Property(P => P.market).IsRequired().ValueGeneratedNever().HasColumnType("bigint").HasMaxLength(50).HasComment("交易对");
             o.Property(P => P.symbol).HasColumnType("nvarchar").HasMaxLength(20).HasComment("交易对名称");
             o.Property(P => P.price_places).IsRequired().HasColumnType("decimal").HasPrecision(28, 16).HasComment("价格小数位数");
             o.Property(P => P.amount_places).IsRequired().HasColumnType("decimal").HasPrecision(28, 16).HasComment("量小数位数");

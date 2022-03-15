75d04939ee864c73eb358e90f8ca0ca778e36e29
fengma_312 <fengma_312@126.com>
2022-03-15 09:26:43 +0800

    a

--- Com.Admin/Controllers/HomeController.cs
+++ Com.Admin/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using Com.Bll;
 using Com.Db;
 using Com.Db.Enum;
 using Com.Db.Model;
+using System.Data;
+using Com.Bll.Util;
 
 namespace Com.Admin.Controllers;
 
@@ -41,37 +43,34 @@ public class HomeController : Controller
 
     public async Task<IActionResult> Index()
     {
-        GrpcChannel channel = GrpcChannel.ForAddress("http://192.168.2.5:8080");
-        var client = new ExchangeService.ExchangeServiceClient(channel);
-
-
-        // var client1 = new Health.HealthClient(channel);
-
-        // var response = client1.CheckAsync(new HealthCheckRequest());
-        // var status = response.Status;
-
-        // inti
-        CallRequest<string> req = new CallRequest<string>();
-        req.op = E_Op.service_warm_cache;
-        req.market = 1;
-        MarketInfo info = new MarketInfo();
-        info.market = 1;
-        info.last_price = 38000.123456789m;
-        req.data = JsonConvert.SerializeObject(info);
-        string json = JsonConvert.SerializeObject(req);
-        var reply = await client.UnaryCallAsync(new Request { Json = json });
-
-        //start
-
-        req.op = E_Op.service_start;
-        req.market = 1;
-        req.data = JsonConvert.SerializeObject(info);
-        json = JsonConvert.SerializeObject(req);
-        var reply2 = await client.UnaryCallAsync(new Request { Json = json });
+        // GrpcChannel channel = GrpcChannel.ForAddress("http://192.168.2.5:8080");
+        // var client = new ExchangeService.ExchangeServiceClient(channel);
 
 
 
-        channel.ShutdownAsync().Wait();
+
+        // // inti
+        // CallRequest<string> req = new CallRequest<string>();
+        // req.op = E_Op.service_warm_cache;
+        // req.market = 1;
+        // MarketInfo info = new MarketInfo();
+        // info.market = 1;
+        // info.last_price = 38000.123456789m;
+        // req.data = JsonConvert.SerializeObject(info);
+        // string json = JsonConvert.SerializeObject(req);
+        // var reply = await client.UnaryCallAsync(new Request { Json = json });
+
+        // //start
+
+        // req.op = E_Op.service_start;
+        // req.market = 1;
+        // req.data = JsonConvert.SerializeObject(info);
+        // json = JsonConvert.SerializeObject(req);
+        // var reply2 = await client.UnaryCallAsync(new Request { Json = json });
+
+
+
+        // channel.ShutdownAsync().Wait();
 
         return View();
     }
@@ -114,4 +113,65 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Upload()
+    {
+        var files = Request.Form.Files.First();
+        MemoryStream ms = new MemoryStream();
+        files.CopyTo(ms);
+        byte[] a = ms.ToArray();
+
+        DataTable dt = ExcelHelper.OpenExcel(files.OpenReadStream(), Path.GetExtension(files.FileName));
+        Dictionary<DateTimeOffset, decimal> dic = new Dictionary<DateTimeOffset, decimal>();
+        foreach (DataRow row in dt.Rows)
+        {
+            dic.Add(DateTimeOffset.Parse(row[0].ToString()), decimal.Parse(row[1].ToString()));
+
+
+        }
+        return Json(new { code = 0, msg = "ok" });
+    }
+
+
+    public void test()
+    {
+        // var date = Request;
+        // var files = Request.Form.Files;
+        // long size = files.Sum(f => f.Length);
+        // string webRootPath = _hostingEnvironment.WebRootPath;
+        // string contentRootPath = _hostingEnvironment.ContentRootPath;
+        // foreach (var formFile in files)
+        // {
+        //     if (formFile.Length > 0)
+        //     {
+
+        //         string fileExt = GetFileExt(formFile.FileName); //文件扩展名，不含“.”
+        //         long fileSize = formFile.Length; //获得文件大小，以字节为单位
+        //         string newFileName = System.Guid.NewGuid().ToString() + "." + fileExt; //随机生成新的文件名
+        //         var filePath = webRootPath + "/upload/" + newFileName;
+        //         using (var stream = new FileStream(filePath, FileMode.Create))
+        //         {
+
+        //             await formFile.CopyToAsync(stream);
+        //         }
+        //     }
+        // }
+
+
+
+
+
+
+    }
+
+    // private DataTable ReadExcelToTable(Stream stream)
+    // {
+    //     DataTable result = new DataTable();
+    //     Workbook workbook = new Workbook();
+    //     workbook.Open(stream);
+    //     Cells cells = workbook.Worksheets[0].Cells;
+    //     result = cells.ExportDataTableAsString(0, 0, cells.MaxDataRow + 1, cells.MaxColumn + 1, false);
+    //     return result;
+    // }
+
 }
---
+++ Com.Bll/Util/ExcelHelper.cs
@@ -0,0 +1,131 @@
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace Com.Bll.Util;
+
+public class ExcelHelper
+{
+
+    /// <summary>
+    /// 读取excel文件
+    /// </summary>
+    /// <param name="FileName"></param>
+    /// <returns></returns>
+    public static DataTable OpenExcel(Stream ms, string fileExt)
+    {
+        DataTable dt = null;
+        // FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+        bool end = false;
+        try
+        {
+            IWorkbook book;
+            // string fileExt = Path.GetExtension(FileName).ToLower();
+            if (fileExt == ".xlsx")
+            {
+                book = new XSSFWorkbook(ms);
+            }
+            else if (fileExt == ".xls")
+            {
+                book = new HSSFWorkbook(ms);
+            }
+            else
+            {
+                book = null;
+                throw new Exception("不认识此Excel文件。");
+            }
+            int sheetCount = book.NumberOfSheets;
+            for (int sheetIndex = 0; sheetIndex < sheetCount; sheetIndex++)
+            {
+                NPOI.SS.UserModel.ISheet sheet = book.GetSheetAt(sheetIndex);
+                if (sheet == null)
+                {
+                    continue;
+                }
+                NPOI.SS.UserModel.IRow row = sheet.GetRow(0);
+                if (row == null)
+                {
+                    continue;
+                }
+                int firstCellNum = row.FirstCellNum;
+                int lastCellNum = row.LastCellNum;
+                if (firstCellNum == lastCellNum)
+                {
+                    continue;
+                }
+                dt = new DataTable(sheet.SheetName);
+                for (int i = firstCellNum; i < lastCellNum; i++)
+                {
+                    dt.Columns.Add(row.GetCell(i).StringCellValue, typeof(string));
+                }
+                for (int i = 1; i <= sheet.LastRowNum; i++)
+                {
+                    DataRow newRow = dt.Rows.Add();
+                    for (int j = firstCellNum; j < lastCellNum; j++)
+                    {
+                        if (sheet.GetRow(i).GetCell(j) != null)
+                        {
+                            try
+                            {
+                                if (sheet.GetRow(i).GetCell(j).CellType == CellType.String)
+                                {
+                                    newRow[j] = sheet.GetRow(i).GetCell(j).StringCellValue;
+                                    newRow[j] = newRow[j].ToString().Trim('\ud8e9').Trim().Replace("\r", "").Replace("\n", "").Replace("\f", "").Replace("\t", "");
+                                }
+                                else if (sheet.GetRow(i).GetCell(j).CellType == CellType.Blank)
+                                {
+                                    newRow[j] = "";
+                                }
+                                else if (sheet.GetRow(i).GetCell(j).CellType == CellType.Numeric)
+                                {
+                                    newRow[j] = sheet.GetRow(i).GetCell(j).NumericCellValue;
+                                }
+                                else if (sheet.GetRow(i).GetCell(j).CellType == CellType.Formula)
+                                {
+                                    newRow[j] = sheet.GetRow(i).GetCell(j).NumericCellValue;//  .getNumericCellValue();
+                                }
+                                else if (sheet.GetRow(i).GetCell(j).CellType == CellType.Boolean)
+                                {
+                                    newRow[j] = sheet.GetRow(i).GetCell(j).BooleanCellValue;
+                                }
+                                else
+                                {
+                                    newRow[j] = sheet.GetRow(i).GetCell(j).ToString();
+                                }
+                                if (j == 1 && newRow[j].ToString() == "以下空白")
+                                {
+                                    end = true;
+                                    break;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+                        }
+                    }
+                    if (end)
+                    {
+                        break;
+                    }
+                }
+                if (end)
+                {
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            // fs.Close();
+        }
+        return dt;
+    }
+
+}

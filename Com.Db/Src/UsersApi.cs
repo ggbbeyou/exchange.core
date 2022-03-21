976771d66e818d09761c93fa69b53b8b41c5f0e1
fengma_312 <fengma_312@126.com>
2022-03-21 22:22:39 +0800

    a

---
+++ Com.Db/Src/UsersApi.cs
@@ -0,0 +1,46 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Com.Db;
+
+/// <summary>
+/// Api用户
+/// </summary>
+public class UsersApi
+{
+    /// <summary>
+    /// 用户id
+    /// </summary>
+    /// <value></value>
+    public long user_id { get; set; }
+    /// <summary>
+    /// 账户key
+    /// </summary>
+    /// <value></value>
+    public string api_key { get; set; } = null!;
+    /// <summary>
+    /// 账户密钥
+    /// </summary>
+    /// <value></value>
+    public string api_secret { get; set; } = null!;
+    /// <summary>
+    /// 是否交易
+    /// </summary>
+    /// <value></value>
+    public bool transaction { get; set; }
+    /// <summary>
+    /// 提现
+    /// </summary>
+    /// <value></value>
+    public bool withdrawal { get; set; }
+    /// <summary>
+    /// IP白名单
+    /// </summary>
+    /// <value></value>
+    public string? white_list_ip { get; set; }
+    /// <summary>
+    /// 最后登录IP地址
+    /// </summary>
+    /// <value></value>
+    public string? last_login_ip { get; set; }
+}

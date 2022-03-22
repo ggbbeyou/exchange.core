500b03ca64f90edf6dc5d14788712b30df3cf98e
fengma <fengma_312@126.com>
2022-03-22 15:25:40 +0800

    a

--- Com.Bll/Src/DepthService.cs
+++ Com.Bll/Src/DepthService.cs
@@ -26,74 +26,7 @@ public class DepthService
     }
 
     /// <summary>
-    /// 转换深度行情
-    /// </summary>
-    /// <param name="bid"></param>
-    /// <param name="orderbook"></param>
-    /// <returns></returns>
-    public Dictionary<E_WebsockerChannel, Depth> ConvertDepth(long market, string symbol, (List<(int, BaseOrderBook)> bid, List<(int, BaseOrderBook)> ask) orderbook)
-    {
-        Dictionary<E_WebsockerChannel, Depth> depths = new Dictionary<E_WebsockerChannel, Depth>();
-        depths.Add(E_WebsockerChannel.books10, new Depth());
-        depths.Add(E_WebsockerChannel.books50, new Depth());
-        depths.Add(E_WebsockerChannel.books200, new Depth());
-        // depths.Add(E_WebsockerChannel.books10_inc, new Depth());
-        // depths.Add(E_WebsockerChannel.books50_inc, new Depth());
-        // depths.Add(E_WebsockerChannel.books200_inc, new Depth());
-        foreach (var item in depths)
-        {
-            item.Value.market = market;
-            item.Value.symbol = symbol;
-            item.Value.timestamp = DateTimeOffset.UtcNow;
-            switch (item.Key)
-            {
-                case E_WebsockerChannel.books10:
-                    item.Value.bid = new decimal[orderbook.bid.Count < 10 ? orderbook.bid.Count : 10, 2];
-                    item.Value.ask = new decimal[orderbook.ask.Count < 10 ? orderbook.ask.Count : 10, 2];
-                    break;
-                case E_WebsockerChannel.books50:
-                    item.Value.bid = new decimal[orderbook.bid.Count < 50 ? orderbook.bid.Count : 50, 2];
-                    item.Value.ask = new decimal[orderbook.ask.Count < 50 ? orderbook.ask.Count : 50, 2];
-
-                    break;
-                case E_WebsockerChannel.books200:
-                    item.Value.bid = new decimal[orderbook.bid.Count < 200 ? orderbook.bid.Count : 200, 2];
-                    item.Value.ask = new decimal[orderbook.ask.Count < 200 ? orderbook.ask.Count : 200, 2];
-                    break;
-                case E_WebsockerChannel.books10_inc:
-
-                    break;
-                case E_WebsockerChannel.books50_inc:
-
-                    break;
-                case E_WebsockerChannel.books200_inc:
-
-                    break;
-                default:
-                    break;
-            }
-            decimal total_bid = 0;
-            decimal total_ask = 0;
-            // for (int i = 0; i < item.Value.bid.GetLength(0); i++)
-            // {
-            //     item.Value.bid[i, 0] = orderbook.bid[i].price;
-            //     item.Value.bid[i, 1] = orderbook.bid[i].amount;
-            //     total_bid += orderbook.bid[i].amount * orderbook.bid[i].price;
-            // }
-            // item.Value.total_bid = total_bid;
-            // for (int i = 0; i < item.Value.ask.GetLength(0); i++)
-            // {
-            //     item.Value.ask[i, 0] = orderbook.ask[i].price;
-            //     item.Value.ask[i, 1] = orderbook.ask[i].amount;
-            //     total_ask += orderbook.ask[i].amount * orderbook.ask[i].price;
-            // }
-            item.Value.total_ask = total_ask;
-        }
-        return depths;
-    }
-
-    /// <summary>
-    /// 转换深度行情
+    /// (全部)转换深度行情
     /// </summary>
     /// <param name="bid"></param>
     /// <param name="orderbook"></param>
@@ -104,9 +37,6 @@ public class DepthService
         depths.Add(E_WebsockerChannel.books10, new Depth());
         depths.Add(E_WebsockerChannel.books50, new Depth());
         depths.Add(E_WebsockerChannel.books200, new Depth());
-        // depths.Add(E_WebsockerChannel.books10_inc, new Depth());
-        // depths.Add(E_WebsockerChannel.books50_inc, new Depth());
-        // depths.Add(E_WebsockerChannel.books200_inc, new Depth());
         foreach (var item in depths)
         {
             item.Value.market = market;
@@ -121,20 +51,10 @@ public class DepthService
                 case E_WebsockerChannel.books50:
                     item.Value.bid = new decimal[orderbook.bid.Count < 50 ? orderbook.bid.Count : 50, 2];
                     item.Value.ask = new decimal[orderbook.ask.Count < 50 ? orderbook.ask.Count : 50, 2];
-
                     break;
                 case E_WebsockerChannel.books200:
                     item.Value.bid = new decimal[orderbook.bid.Count < 200 ? orderbook.bid.Count : 200, 2];
                     item.Value.ask = new decimal[orderbook.ask.Count < 200 ? orderbook.ask.Count : 200, 2];
-                    break;
-                case E_WebsockerChannel.books10_inc:
-
-                    break;
-                case E_WebsockerChannel.books50_inc:
-
-                    break;
-                case E_WebsockerChannel.books200_inc:
-
                     break;
                 default:
                     break;
@@ -160,7 +80,58 @@ public class DepthService
     }
 
     /// <summary>
-    /// 深度行情保存到redis并且推送到MQ
+    /// (部分)转换深度行情
+    /// </summary>
+    /// <param name="bid"></param>
+    /// <param name="orderbook"></param>
+    /// <returns></returns>
+    public Dictionary<E_WebsockerChannel, Depth> ConvertDepth(long market, string symbol, (List<(int, BaseOrderBook)> bid, List<(int, BaseOrderBook)> ask) orderbook)
+    {
+        Dictionary<E_WebsockerChannel, Depth> depths = new Dictionary<E_WebsockerChannel, Depth>();
+        depths.Add(E_WebsockerChannel.books10_inc, new Depth());
+        depths.Add(E_WebsockerChannel.books50_inc, new Depth());
+        depths.Add(E_WebsockerChannel.books200_inc, new Depth());
+        foreach (var item in depths)
+        {
+            item.Value.market = market;
+            item.Value.symbol = symbol;
+            item.Value.timestamp = DateTimeOffset.UtcNow;
+            switch (item.Key)
+            {
+                case E_WebsockerChannel.books10_inc:
+                    item.Value.bid = new decimal[orderbook.bid.Count < 10 ? orderbook.bid.Count : 10, 2];
+                    item.Value.ask = new decimal[orderbook.ask.Count < 10 ? orderbook.ask.Count : 10, 2];
+                    break;
+                case E_WebsockerChannel.books50_inc:
+                    item.Value.bid = new decimal[orderbook.bid.Count < 50 ? orderbook.bid.Count : 50, 2];
+                    item.Value.ask = new decimal[orderbook.ask.Count < 50 ? orderbook.ask.Count : 50, 2];
+                    break;
+                case E_WebsockerChannel.books200_inc:
+                    item.Value.bid = new decimal[orderbook.bid.Count < 200 ? orderbook.bid.Count : 200, 2];
+                    item.Value.ask = new decimal[orderbook.ask.Count < 200 ? orderbook.ask.Count : 200, 2];
+                    break;
+                default:
+                    break;
+            }
+            // for (int i = 0; i < item.Value.bid.GetLength(0); i++)
+            // {
+            //     item.Value.bid[i, 0] = orderbook.bid[i].price;
+            //     item.Value.bid[i, 1] = orderbook.bid[i].amount;
+            //     total_bid += orderbook.bid[i].amount * orderbook.bid[i].price;
+            // }
+
+            // for (int i = 0; i < item.Value.ask.GetLength(0); i++)
+            // {
+            //     item.Value.ask[i, 0] = orderbook.ask[i].price;
+            //     item.Value.ask[i, 1] = orderbook.ask[i].amount;
+            //     total_ask += orderbook.ask[i].amount * orderbook.ask[i].price;
+            // }
+        }
+        return depths;
+    }
+
+    /// <summary>
+    /// (全部)深度行情保存到redis并且推送到MQ
     /// </summary>
     /// <param name="depth"></param>
     public void Push(Dictionary<E_WebsockerChannel, Depth> depths)
@@ -178,7 +149,25 @@ public class DepthService
     }
 
     /// <summary>
-    /// 深度差异
+    /// (部分)深度行情保存到redis并且推送到MQ
+    /// </summary>
+    /// <param name="depth"></param>
+    public void PushDiff(Dictionary<E_WebsockerChannel, Depth> depths)
+    {
+        ResWebsocker<Depth> resWebsocker = new ResWebsocker<Depth>();
+        resWebsocker.success = true;
+        resWebsocker.op = E_WebsockerOp.subscribe_date;
+        foreach (var item in depths)
+        {
+            FactoryService.instance.constant.redis.HashSet(FactoryService.instance.GetRedisDepth(item.Value.market), item.Key.ToString(), JsonConvert.SerializeObject(item.Value));
+            resWebsocker.channel = item.Key;
+            resWebsocker.data = item.Value;
+            FactoryService.instance.constant.MqPublish(FactoryService.instance.GetMqSubscribe(item.Key, item.Value.market), JsonConvert.SerializeObject(resWebsocker));
+        }
+    }
+
+    /// <summary>
+    /// (部分)深度差异
     /// </summary>
     /// <param name="bid"></param>
     /// <param name="bid"></param>
@@ -216,7 +205,7 @@ public class DepthService
     }
 
     /// <summary>
-    /// 差异
+    /// (部分)差异
     /// </summary>
     /// <param name="book_old"></param>
     /// <param name="book_new"></param>
@@ -282,24 +271,5 @@ public class DepthService
         }
         return diff;
     }
-
-    /// <summary>
-    /// 深度行情保存到redis并且推送到MQ
-    /// </summary>
-    /// <param name="depth"></param>
-    public void PushDiff(Dictionary<E_WebsockerChannel, Depth> depths)
-    {
-        ResWebsocker<Depth> resWebsocker = new ResWebsocker<Depth>();
-        resWebsocker.success = true;
-        resWebsocker.op = E_WebsockerOp.subscribe_date;
-        foreach (var item in depths)
-        {
-            FactoryService.instance.constant.redis.HashSet(FactoryService.instance.GetRedisDepth(item.Value.market), item.Key.ToString(), JsonConvert.SerializeObject(item.Value));
-            resWebsocker.channel = item.Key;
-            resWebsocker.data = item.Value;
-            FactoryService.instance.constant.MqPublish(FactoryService.instance.GetMqSubscribe(item.Key, item.Value.market), JsonConvert.SerializeObject(resWebsocker));
-        }
-    }
-
 
 }

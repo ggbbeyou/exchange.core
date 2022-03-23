a91f5e9c77a4fe59465b3278422706e8f65391a2
fengma_312 <fengma_312@126.com>
2022-03-23 23:10:27 +0800

    a

--- Com.Service/Match/MatchCore.cs
+++ Com.Service/Match/MatchCore.cs
@@ -182,7 +182,7 @@ public class MatchCore
         }
         List<Orders> cancel = new List<Orders>();
         List<Orders> bid_market = this.market_bid.Where(P => P.trigger_cancel_price > 0 && P.trigger_cancel_price >= price).ToList();
-        bid_market.ForEach(P => { P.state = E_OrderState.cancel; P.deal_last_time = DateTimeOffset.UtcNow; P.remarks = "买单高于撤单触发价,系统自动撤单"; });
+        bid_market.ForEach(P => { P.state = E_OrderState.cancel; P.deal_last_time = DateTimeOffset.UtcNow; P.remarks = "买单高于撤单触发价,或市价买价余额不足时,系统自动撤单"; });
         cancel.AddRange(bid_market);
         List<Orders> ask_market = this.market_ask.Where(P => P.trigger_cancel_price > 0 && P.trigger_cancel_price <= price).ToList();
         ask_market.ForEach(P => { P.state = E_OrderState.cancel; P.deal_last_time = DateTimeOffset.UtcNow; P.remarks = "卖单已低于撤单触发价,系统自动撤单"; });

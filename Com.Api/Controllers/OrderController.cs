59d12292a70a219a45c5aae56eb34d04f41be100
fengma <fengma_312@126.com>
2022-03-24 11:49:29 +0800

    a

--- Com.Api/Controllers/OrderController.cs
+++ Com.Api/Controllers/OrderController.cs
@@ -38,11 +38,11 @@ public class OrderController : Controller
     /// 交易对基础信息
     /// </summary>
     /// <returns></returns>
-    public ServiceMarket market_info_db = new ServiceMarket();
+    public ServiceMarket service_market = new ServiceMarket();
     /// <summary>
     /// Service:订单
     /// </summary>
-    public ServiceOrder order_service = new ServiceOrder();
+    public ServiceOrder service_order = new ServiceOrder();
 
     /// <summary>
     /// 
@@ -66,47 +66,8 @@ public class OrderController : Controller
     [HttpPost]
     public IActionResult PlaceOrder(string symbol, List<ReqOrder> orders)
     {
-        ReqCall<List<Orders>> result = new ReqCall<List<Orders>>();
-        List<Orders> matchOrders = new List<Orders>();
-        Market? market = this.market_info_db.GetMarketBySymbol(symbol);
-        if (market == null)
-        {
-
-        }
-        else
-        {
-            foreach (var item in orders)
-            {
-                Orders orderResult = new Orders();
-                orderResult.order_id = FactoryService.instance.constant.worker.NextId();
-                orderResult.market = market.market;
-                orderResult.symbol = market.symbol;
-                orderResult.client_id = item.client_id;
-                orderResult.uid = user_id;
-                orderResult.price = item.price ?? 0;
-                orderResult.amount = item.amount;
-                orderResult.total = item.price ?? 0 * item.amount;
-                orderResult.create_time = DateTimeOffset.UtcNow;
-                // orderResult.amount_unsold = item.amount;
-                orderResult.amount_done = 0;
-                orderResult.deal_last_time = null;
-                orderResult.side = item.side;
-                orderResult.state = E_OrderState.unsold;
-                orderResult.type = item.type;
-                orderResult.trigger_hanging_price = 0;
-                orderResult.trigger_cancel_price = 0;
-                orderResult.data = null;
-                orderResult.remarks = null;
-                matchOrders.Add(orderResult);
-            }
-            // Res<List<Orders>> res = this.order_service.PlaceOrder(market, user_id, matchOrders);
-            result.data = new List<Orders>();
-            // foreach (var item in res.data)
-            // {
-            //     result.data.Add(item);
-            // }
-        }
-        return Json(result);
+        //判断用户api是否有交易权限
+        return Json(service_order.PlaceOrder(symbol, user_id, orders));
     }
 
     /// <summary>
@@ -124,7 +85,7 @@ public class OrderController : Controller
         {
             return Json(call_res);
         }
-        ResCall<KeyValuePair<long, List<long>>> res = this.order_service.CancelOrder(market, user_id, type, data);
+        ResCall<KeyValuePair<long, List<long>>> res = this.service_order.CancelOrder(market, user_id, type, data);
         return Json(res);
     }
 
@@ -133,49 +94,6 @@ public class OrderController : Controller
     //       /////////////////////////////////////////////////
 
 
-    [HttpPost]
-    public IActionResult PlaceOrderText()
-    {
-        // List<PlaceOrder> orders = new List<PlaceOrder>();
-        // Random r = new Random();
-        // for (int i = 0; i < 500; i++)
-        // {
-        //     PlaceOrder orderResult = new PlaceOrder();
-        //     orderResult.side = r.Next(0, 2) == 0 ? E_OrderSide.buy : E_OrderSide.sell;
-        //     orderResult.type = r.Next(0, 2) == 0 ? E_OrderType.price_limit : E_OrderType.price_market;
-        //     orderResult.client_id = null;
-        //     if (orderResult.type == E_OrderType.price_limit)
-        //     {
-        //         orderResult.price = (decimal)FactoryService.instance.constant.random.Next(1, 30);
-        //     }
-        //     else
-        //     {
-        //         orderResult.price = null;
-        //     }
-        //     orderResult.amount = (decimal)FactoryService.instance.constant.random.Next(1, 30);
-        //     orders.Add(orderResult);
-        // }
-        // PlaceOrder("eth/usdt", orders);
-        // List<PlaceOrder> orders1 = new List<PlaceOrder>();
-        // for (int i = 0; i < 500; i++)
-        // {
-        //     PlaceOrder orderResult = new PlaceOrder();
-        //     orderResult.side = r.Next(0, 2) == 0 ? E_OrderSide.buy : E_OrderSide.sell;
-        //     orderResult.type = r.Next(0, 2) == 0 ? E_OrderType.price_limit : E_OrderType.price_market;
-        //     orderResult.client_id = null;
-        //     if (orderResult.type == E_OrderType.price_limit)
-        //     {
-        //         orderResult.price = (decimal)FactoryService.instance.constant.random.Next(1, 30);
-        //     }
-        //     else
-        //     {
-        //         orderResult.price = null;
-        //     }
-        //     orderResult.amount = (decimal)FactoryService.instance.constant.random.Next(1, 30);
-        //     orders1.Add(orderResult);
-        // }
-        // PlaceOrder("btc/usdt", orders);
-        return Json(new { });
-    }
+   
 
 }
---
+++ Com.Api/Controllers/TestController.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Com.Api.Models;
+using Com.Bll;
+using Com.Db;
+using Com.Api.Sdk.Enum;
+using Com.Db.Model;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Com.Api.Sdk.Models;
+
+namespace Com.Api.Controllers;
+
+// [Route("api/[controller]/[action]")]
+// [Authorize]
+[AllowAnonymous]
+public class TestController : Controller
+{
+    public DbContextEF db = null!;
+
+    /// <summary>
+    /// 登录玩家id
+    /// </summary>
+    /// <value></value>
+    public int user_id
+    {
+        get
+        {
+            Claim? claim = User.Claims.FirstOrDefault(P => P.Type == JwtRegisteredClaimNames.Aud);
+            if (claim != null)
+            {
+                return Convert.ToInt32(claim.Value);
+            }
+            return 5;
+        }
+    }
+
+    /// <summary>
+    /// 交易对基础信息
+    /// </summary>
+    /// <returns></returns>
+    public ServiceMarket market_info_db = new ServiceMarket();
+    /// <summary>
+    /// Service:订单
+    /// </summary>
+    public ServiceOrder order_service = new ServiceOrder();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="configuration"></param>
+    /// <param name="environment"></param>
+    /// <param name="provider"></param>
+    /// <param name="logger"></param>
+    public TestController(IServiceProvider provider, IConfiguration configuration, IHostEnvironment environment, DbContextEF db, ILogger<OrderController> logger)
+    {
+        this.db = db;
+    }
+
+    public IActionResult Init()
+    {
+        Coin btc = new Coin()
+        {
+
+        };
+
+        return View();
+    }
+
+
+    [HttpPost]
+    public IActionResult PlaceOrderText()
+    {
+        // List<PlaceOrder> orders = new List<PlaceOrder>();
+        // Random r = new Random();
+        // for (int i = 0; i < 500; i++)
+        // {
+        //     PlaceOrder orderResult = new PlaceOrder();
+        //     orderResult.side = r.Next(0, 2) == 0 ? E_OrderSide.buy : E_OrderSide.sell;
+        //     orderResult.type = r.Next(0, 2) == 0 ? E_OrderType.price_limit : E_OrderType.price_market;
+        //     orderResult.client_id = null;
+        //     if (orderResult.type == E_OrderType.price_limit)
+        //     {
+        //         orderResult.price = (decimal)FactoryService.instance.constant.random.Next(1, 30);
+        //     }
+        //     else
+        //     {
+        //         orderResult.price = null;
+        //     }
+        //     orderResult.amount = (decimal)FactoryService.instance.constant.random.Next(1, 30);
+        //     orders.Add(orderResult);
+        // }
+        // PlaceOrder("eth/usdt", orders);
+        // List<PlaceOrder> orders1 = new List<PlaceOrder>();
+        // for (int i = 0; i < 500; i++)
+        // {
+        //     PlaceOrder orderResult = new PlaceOrder();
+        //     orderResult.side = r.Next(0, 2) == 0 ? E_OrderSide.buy : E_OrderSide.sell;
+        //     orderResult.type = r.Next(0, 2) == 0 ? E_OrderType.price_limit : E_OrderType.price_market;
+        //     orderResult.client_id = null;
+        //     if (orderResult.type == E_OrderType.price_limit)
+        //     {
+        //         orderResult.price = (decimal)FactoryService.instance.constant.random.Next(1, 30);
+        //     }
+        //     else
+        //     {
+        //         orderResult.price = null;
+        //     }
+        //     orderResult.amount = (decimal)FactoryService.instance.constant.random.Next(1, 30);
+        //     orders1.Add(orderResult);
+        // }
+        // PlaceOrder("btc/usdt", orders);
+        return Json(new { });
+    }
+
+}

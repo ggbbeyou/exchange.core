a3c75382d80e880573d57cc375eecb7c86ec1bde
fengma_312 <fengma_312@126.com>
2022-03-27 21:44:33 +0800

    a

--- Com.Api.Sdk/Enum/E_Op.cs
+++ Com.Api.Sdk/Enum/E_Op.cs
@@ -12,40 +12,32 @@ public enum E_Op
     /// </summary>
     service_get_status = 0,
     /// <summary>
-    /// 服务初始化  动作:清除缓存
-    /// </summary>
-    // service_clear_cache = 1,
-    /// <summary>
-    /// 服务初始化  动作:预热缓存,如同步K线或成交记录到redis
-    /// </summary>
-    // service_warm_cache = 2,
-    /// <summary>
     /// 服务启动  动作:启动服务,如启动撮合成交后续动作
     /// </summary>
-    service_start = 3,
+    service_start = 1,
     /// <summary>
     /// 服务停止
     /// </summary>
-    service_stop = 4,
+    service_stop = 2,
     /// <summary>
     /// 挂单
     /// </summary>
-    place = 5,
+    place = 3,
     /// <summary>
     /// 根据订单id撤单
     /// </summary>
-    cancel_by_id = 6,
+    cancel_by_id = 4,
     /// <summary>
     /// 根据用户id撤单
     /// </summary>
-    cancel_by_uid = 7,
+    cancel_by_uid = 5,
     /// <summary>
     /// 根据客户自定义id撤单
     /// </summary>
-    cancel_by_clientid = 8,
+    cancel_by_clientid = 6,
     /// <summary>
     /// 该交易对全部撤单
     /// </summary>
-    cancel_by_all = 9,
+    cancel_by_all = 7,
 
 }

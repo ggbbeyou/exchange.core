2b55481652afd301e0c9c5da95cfcbd71509c2e3
fengma_312 <fengma_312@126.com>
2022-04-01 20:51:00 +0800

    a

--- Com.Service/Src/FactoryMatching.cs
+++ Com.Service/Src/FactoryMatching.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using Com.Api.Sdk.Enum;
 using Com.Bll;
 using Com.Db;
 using Com.Service.Match;
 using Com.Service.Models;
+using Microsoft.Extensions.Logging;
 
 namespace Com.Service;
 
@@ -41,6 +43,12 @@ public class FactoryMatching
     /// <returns></returns>
     private Mutex mutex = new Mutex(false);
     /// <summary>
+    /// 秒表
+    /// </summary>
+    /// <returns></returns>
+    public Stopwatch stopwatch = new Stopwatch();
+
+    /// <summary>
     /// 私有构造方法
     /// </summary>
     private FactoryMatching()
@@ -71,42 +79,44 @@ public class FactoryMatching
     /// <param name="info"></param>
     public Market ServiceStart(Market info)
     {
+        FactoryService.instance.constant.logger.LogInformation($"服务准备启动:{info.market}");
         this.mutex.WaitOne();
         if (!this.service.ContainsKey(info.market))
         {
-            MatchModel model = new MatchModel(info);
-            model.match_core = new MatchCore(model);
-            model.mq = new MQ(model);
-            model.core = new Core(model);
-            this.service.Add(info.market, model);
+            MatchModel mm = new MatchModel(info);
+            mm.match_core = new MatchCore(mm);
+            mm.mq = new MQ(mm);
+            mm.core = new Core(mm);
+            this.service.Add(info.market, mm);
         }
-        MatchModel mm = this.service[info.market];
-        if (mm.run == false)
+        MatchModel model = this.service[info.market];
+        if (model.run == false)
         {
             ServiceClearCache(info);
             ServiceWarmCache(info);
-            mm.run = true;
+            model.run = true;
             (string queue_name, string consume_tag) order_cancel = this.service[info.market].mq.OrderCancel();
-            if (!mm.mq_queues.Contains(order_cancel.queue_name))
+            if (!model.mq_queues.Contains(order_cancel.queue_name))
             {
-                mm.mq_queues.Add(order_cancel.queue_name);
+                model.mq_queues.Add(order_cancel.queue_name);
             }
-            if (!mm.mq_consumer.Contains(order_cancel.consume_tag))
+            if (!model.mq_consumer.Contains(order_cancel.consume_tag))
             {
-                mm.mq_consumer.Add(order_cancel.consume_tag);
+                model.mq_consumer.Add(order_cancel.consume_tag);
             }
             (string queue_name, string consume_tag) order_receive = this.service[info.market].mq.OrderReceive();
-            if (!mm.mq_queues.Contains(order_receive.queue_name))
+            if (!model.mq_queues.Contains(order_receive.queue_name))
             {
-                mm.mq_queues.Add(order_receive.queue_name);
+                model.mq_queues.Add(order_receive.queue_name);
             }
-            if (!mm.mq_consumer.Contains(order_receive.consume_tag))
+            if (!model.mq_consumer.Contains(order_receive.consume_tag))
             {
-                mm.mq_consumer.Add(order_receive.consume_tag);
+                model.mq_consumer.Add(order_receive.consume_tag);
             }
         }
-        info.status = mm.run;
+        info.status = model.run;
         this.mutex.ReleaseMutex();
+        FactoryService.instance.constant.logger.LogInformation(model.eventId, $"服务启动成功:{info.market}");
         return info;
     }
 
@@ -116,6 +126,7 @@ public class FactoryMatching
     /// <param name="info"></param>
     public Market ServiceStop(Market info)
     {
+        FactoryService.instance.constant.logger.LogInformation($"服务准备关闭:{info.market}");
         this.mutex.WaitOne();
         if (this.service.ContainsKey(info.market))
         {
@@ -130,6 +141,7 @@ public class FactoryMatching
             info.status = false;
         }
         this.mutex.ReleaseMutex();
+        FactoryService.instance.constant.logger.LogInformation($"服务关闭成功:{info.market}");
         return info;
     }
 
@@ -156,9 +168,12 @@ public class FactoryMatching
             mm.match_core.CancelOrder();
         }
         //交易记录数据从DB同步到Redis 至少保存最近3个月记录
+        FactoryService.instance.constant.stopwatch.Restart();
         long delete = this.deal_service.DeleteDeal(info.market, DateTimeOffset.UtcNow.AddMonths(-2));
         ServiceDepth.instance.DeleteRedisDepth(info.market);
         kline_service.DeleteRedisKline(info.market);
+        FactoryService.instance.constant.stopwatch.Stop();
+        FactoryService.instance.constant.logger.LogTrace($"计算耗时:{FactoryService.instance.constant.stopwatch.Elapsed.ToString()};{info.symbol}:redis=>清除所有缓存");
         return true;
     }
 
@@ -167,6 +182,7 @@ public class FactoryMatching
     /// </summary>
     private bool ServiceWarmCache(Market info)
     {
+        FactoryService.instance.constant.stopwatch.Restart();
         DateTimeOffset now = DateTimeOffset.UtcNow;
         now = now.AddSeconds(-now.Second).AddMilliseconds(-now.Millisecond);
         this.deal_service.DealDbToRedis(info.market, now.AddMonths(-2));
@@ -174,6 +190,8 @@ public class FactoryMatching
         this.kline_service.DBtoRedised(info.market, info.symbol, end);
         this.kline_service.DBtoRedising(info.market, info.symbol);
         order_service.PushOrderToMqRedis(info.market);
+        FactoryService.instance.constant.stopwatch.Stop();
+        FactoryService.instance.constant.logger.LogTrace($"计算耗时:{FactoryService.instance.constant.stopwatch.Elapsed.ToString()};{info.symbol}:redis=>预热缓存");
         return true;
     }
 

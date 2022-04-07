cf564fdc5dc60f0820c9fcce99cec98f7a01647e
fengma_312 <fengma_312@126.com>
2022-04-07 21:14:54 +0800

    a

---
+++ Com.Api/Controllers/WalletController.cs
@@ -0,0 +1,72 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Com.Api.Sdk.Enum;
+using Com.Api.Sdk.Models;
+using Com.Bll;
+using Com.Db;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Com.Api.Controllers;
+
+/// <summary>
+/// 钱包接口
+/// </summary>
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public class WalletController : ControllerBase
+{
+    /// <summary>
+    /// 日志
+    /// </summary>
+    private readonly ILogger<WalletController> logger;
+    /// <summary>
+    /// 登录信息
+    /// </summary>
+    private (long user_id, long no, string user_name, string app, string public_key) login
+    {
+        get
+        {
+            return this.service_common.GetLoginUser(User);
+        }
+    }
+    /// <summary>
+    /// service:公共服务
+    /// </summary>
+    private ServiceCommon service_common = new ServiceCommon();
+    /// <summary>
+    /// 用户服务
+    /// </summary>
+    /// <returns></returns>
+    private ServiceUser service_user = new ServiceUser();
+    /// <summary>
+    /// Service:钱包
+    /// </summary>
+    private ServiceWallet service_wallet = new ServiceWallet();
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    /// <param name="logger"></param>
+    public WalletController(ILogger<WalletController> logger)
+    {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// 划转
+    /// </summary>
+    /// <param name="coin_id"></param>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("Transfer")]
+    public Res<bool> Transfer(long coin_id, E_WalletType from, E_WalletType to, decimal amount)
+    {
+        return service_wallet.Transfer(login.user_id, coin_id, from, to, amount);
+    }
+
+}

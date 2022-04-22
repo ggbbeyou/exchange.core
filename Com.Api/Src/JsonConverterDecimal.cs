fa48a9cae7feb31e1b50aa8dcc130a8ed086cfa3
fengma_312 <fengma_312@126.com>
2022-04-22 23:03:15 +0800

    a

---
+++ Com.Api/Src/JsonConverterDecimal.cs
@@ -0,0 +1,38 @@
+
+
+using Newtonsoft.Json;
+
+namespace Com.Api;
+
+/// <summary>
+/// json 转换器，主要是去掉decimal小数点后面多余的0
+/// </summary>
+public class JsonConverterDecimal : JsonConverter<decimal>
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="objectType"></param>
+    /// <param name="existingValue"></param>
+    /// <param name="hasExistingValue"></param>
+    /// <param name="serializer"></param>
+    /// <returns></returns>
+    public override decimal ReadJson(JsonReader reader, Type objectType, decimal existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        return existingValue;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="serializer"></param>
+    public override void WriteJson(JsonWriter writer, decimal value, JsonSerializer serializer)
+    {
+        double input = Convert.ToDouble(value);
+        writer.WriteValue((decimal)input);
+    }
+}
--- Com.Api/Src/MainService.cs
+++ Com.Api/Src/MainService.cs
@@ -8,54 +8,53 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging.Abstractions;
 using Com.Bll;
 
-namespace Com.Api
+namespace Com.Api;
+
+/// <summary>
+/// 网站后台服务
+/// </summary>
+public class MainService : BackgroundService
 {
     /// <summary>
-    /// 网站后台服务
+    /// 常用接口
     /// </summary>
-    public class MainService : BackgroundService
+    public FactoryConstant constant = null!;
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    /// <param name="provider">驱动</param>
+    /// <param name="configuration">配置接口</param>
+    /// <param name="environment">环境接口</param>
+    /// <param name="logger">日志接口</param>
+    public MainService(IServiceProvider provider, IConfiguration configuration, IHostEnvironment environment, ILogger<MainService> logger)
     {
-        /// <summary>
-        /// 常用接口
-        /// </summary>
-        public FactoryConstant constant = null!;
-
-        /// <summary>
-        /// 初始化
-        /// </summary>
-        /// <param name="provider">驱动</param>
-        /// <param name="configuration">配置接口</param>
-        /// <param name="environment">环境接口</param>
-        /// <param name="logger">日志接口</param>
-        public MainService(IServiceProvider provider, IConfiguration configuration, IHostEnvironment environment, ILogger<MainService> logger)
-        {
-            this.constant = new FactoryConstant(provider, configuration, environment, logger ?? NullLogger<MainService>.Instance);
-
-        }
-
-        /// <summary>
-        /// 任务执行方法
-        /// </summary>
-        /// <param name="stoppingToken">后台任务取消令牌</param>
-        /// <returns></returns>
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            this.constant.logger.LogInformation("准备启动Api后台服务");
-            try
-            {
-                FactoryService.instance.Init(this.constant);
-                ServiceMinio service_minio = new ServiceMinio(this.constant.config, this.constant.logger);
-                await service_minio.MakeBucket(FactoryService.instance.GetMinioRealname());
-
-
-                this.constant.logger.LogInformation("启动Api后台服务成功");
-            }
-            catch (Exception ex)
-            {
-                this.constant.logger.LogError(ex, "启动Api后台服务异常");
-            }
-            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
-        }
+        this.constant = new FactoryConstant(provider, configuration, environment, logger ?? NullLogger<MainService>.Instance);
 
     }
+
+    /// <summary>
+    /// 任务执行方法
+    /// </summary>
+    /// <param name="stoppingToken">后台任务取消令牌</param>
+    /// <returns></returns>
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        this.constant.logger.LogInformation("准备启动Api后台服务");
+        try
+        {
+            FactoryService.instance.Init(this.constant);
+            ServiceMinio service_minio = new ServiceMinio(this.constant.config, this.constant.logger);
+            await service_minio.MakeBucket(FactoryService.instance.GetMinioRealname());
+
+
+            this.constant.logger.LogInformation("启动Api后台服务成功");
+        }
+        catch (Exception ex)
+        {
+            this.constant.logger.LogError(ex, "启动Api后台服务异常");
+        }
+        await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+    }
+
 }

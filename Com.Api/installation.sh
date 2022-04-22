a244b0cf3c735bed190d9a891c5211567c70d9c7
fengma <fengma_312@126.com>
2022-04-22 14:53:27 +0800

    a

--- Com.Api/installation.sh
+++ Com.Api/installation.sh
@@ -2,8 +2,8 @@
 echo 准备安装服务
 cd /github/exchange.core/Com.Api/
 git pull
-docker stop api
+docker stop e_api
 docker run --rm -v /github/exchange.core/:/app -w /app mcr.microsoft.com/dotnet/sdk:6.0 dotnet publish -c Release /app/Com.Api/Com.Api.csproj
-docker start api
-#docker run -d -p 8001:80 --restart=always  --name api -v /github/exchange.core/Com.Api/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/aspnet:6.0 dotnet Com.Api.dll
+docker start e_api
+#docker run -d -p 8002:80 --restart=always  --name e_api -v /github/exchange.core/Com.Api/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/aspnet:6.0 dotnet Com.Api.dll
 echo 更新成功

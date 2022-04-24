4319457509cc38f19c5262d31a60b4cf9ce3ad2d
fengma <fengma_312@126.com>
2022-04-24 17:16:14 +0800

    a

--- Com.Service/installation.sh
+++ Com.Service/installation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo 准备安装服务
-cd /gitlab/exchange-core/Com.Service/
+cd /gitlab/exchange.core/Com.Service/
 git pull
 docker stop e_service
-docker run --rm -v /gitlab/exchange-core/:/app -w /app mcr.microsoft.com/dotnet/sdk:6.0 dotnet publish -c Release /app/Com.Service/Com.Service.csproj
+docker run --rm -v /gitlab/exchange.core/:/app -w /app mcr.microsoft.com/dotnet/sdk:6.0 dotnet publish -c Release /app/Com.Service/Com.Service.csproj
 docker start e_service
-#docker run -d -p 8000:8000 --restart=always  --name e_service -v /gitlab/exchange-core/Com.Service/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/runtime:6.0 dotnet Com.Service.dll
+#docker run -d -p 8000:8000 --restart=always  --name e_service -v /gitlab/exchange.core/Com.Service/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/runtime:6.0 dotnet Com.Service.dll
 echo 更新成功
 

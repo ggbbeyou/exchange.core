62b1ccaffa2a179d777c6cf0fd29c32d7f1910c1
fengma_312 <fengma_312@126.com>
2022-04-25 23:08:08 +0800

    a

--- Com.Api.Admin/installation.sh
+++ Com.Api.Admin/installation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo 准备安装服务
-cd /gitlab/exchange.core/Com.Api.Admin/
+cd /github/exchange.core/Com.Api.Admin/
 git pull
 docker stop e_admin
-docker run --rm -v /gitlab/exchange.core/:/app -w /app mcr.microsoft.com/dotnet/sdk:6.0 dotnet publish -c Release /app/Com.Api.Admin/Com.Api.Admin.csproj
+docker run --rm -v /github/exchange.core/:/app -w /app mcr.microsoft.com/dotnet/sdk:6.0 dotnet publish -c Release /app/Com.Api.Admin/Com.Api.Admin.csproj
 docker start e_admin
-#docker run -d -p 8001:80 --restart=always  --name e_admin -v /gitlab/exchange.core/Com.Api.Admin/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/aspnet:6.0 dotnet Com.Api.Admin.dll
+#docker run -d -p 8001:80 --restart=always  --name e_admin -v /github/exchange.core/Com.Api.Admin/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/aspnet:6.0 dotnet Com.Api.Admin.dll
 echo 更新成功
 
 
-docker run -d -p 8001:80 --restart=always  --name e_admin -v /gitlab/exchange.core/Com.Api.Admin/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/runtime:6.0 dotnet Com.Api.Admin.dll
+docker run -d -p 8001:80 --restart=always  --name e_admin -v /github/exchange.core/Com.Api.Admin/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/runtime:6.0 dotnet Com.Api.Admin.dll
--- Com.Api/installation.sh
+++ Com.Api/installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 准备安装服务
-cd /gitlab/exchange.core/Com.Api/
+cd /github/exchange.core/Com.Api/
 git pull
 docker stop e_api
-docker run --rm -v /gitlab/exchange.core/:/app -w /app mcr.microsoft.com/dotnet/sdk:6.0 dotnet publish -c Release /app/Com.Api/Com.Api.csproj
+docker run --rm -v /github/exchange.core/:/app -w /app mcr.microsoft.com/dotnet/sdk:6.0 dotnet publish -c Release /app/Com.Api/Com.Api.csproj
 docker start e_api
-#docker run -d -p 8002:80 --restart=always  --name e_api -v /gitlab/exchange.core/Com.Api/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/aspnet:6.0 dotnet Com.Api.dll
+#docker run -d -p 8002:80 --restart=always  --name e_api -v /github/exchange.core/Com.Api/bin/Release/net6.0/publish/:/app -w /app mcr.microsoft.com/dotnet/aspnet:6.0 dotnet Com.Api.dll
 echo 更新成功
